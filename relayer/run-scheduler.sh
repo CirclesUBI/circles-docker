c169e4361b1d71af10bcf325603dffaad73b41a0
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-09-10 12:30:18 +0200

    Start scheduler later as it is showing errors before db migrations

--- relayer/run-scheduler.sh
+++ relayer/run-scheduler.sh
@@ -9,5 +9,8 @@ else
     log_level="info"
 fi
 
+# Wait for db migrations
+sleep 10
+
 echo "==> Running Celery beat <=="
 exec celery beat -A safe_relay_service.taskapp -S django_celery_beat.schedulers:DatabaseScheduler --loglevel $log_level
