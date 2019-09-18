a7f9a39ad6eec23c12c9d6e9f044e6c1811d81b5
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-09-18 06:50:49 +0200

    Rename env vars and remove POSTGRES_PORT

--- username-service/run.sh
+++ username-service/run.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # Wait for postgres to be ready
-while ! PGPASSWORD=$POSTGRES_PASSWORD pg_isready -h "db" -U "$POSTGRES_USER" -d "$POSTGRES_DATABASE_USERNAME_RESOLVER" -p "$POSTGRES_PORT" > /dev/null 2> /dev/null; do
+while ! PGPASSWORD=$POSTGRES_PASSWORD pg_isready -h "db" -U "$POSTGRES_USER" -d "$POSTGRES_DATABASE_USERNAME_RESOLVER" > /dev/null 2> /dev/null; do
   echo "Waiting for database to be ready ..."
   sleep 5
 done
 
 >&2 echo "Database is ready!"
 
-export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@db:$POSTGRES_PORT/$POSTGRES_DATABASE_USERNAME_RESOLVER
+export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@db:5432/$POSTGRES_DATABASE_USERNAME_RESOLVER
 export DATABASE_DIALECT=postgres
 
 export NODE_ENV=production
