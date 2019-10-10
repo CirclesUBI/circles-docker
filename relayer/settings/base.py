9f971c14e00eebca1a78cb12018862159c69bbc2
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-10-11 00:26:34 +0200

    Add CORS headers

--- relayer/settings/base.py
+++ relayer/settings/base.py
@@ -43,10 +43,11 @@ DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.messages',
     'django.contrib.sessions',
     'django.contrib.sites',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 THIRD_PARTY_APPS = [
@@ -66,6 +67,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 # MIDDLEWARE
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
