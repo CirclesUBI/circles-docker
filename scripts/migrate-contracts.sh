b0873a2382da4760c37f514c9133d9ecac141548
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-12-11 16:32:43 +0100

    Use master branch of contracts

--- scripts/migrate-contracts.sh
+++ scripts/migrate-contracts.sh
@@ -9,9 +9,12 @@ check_tmp_folder $REPOSITORY $FOLDER_NAME
 
 # Clean up
 rm -rf build
-git checkout 89f316ab7bb7a6779b68709a1d7d7824397d2870
-# git fetch --all
-# git reset --hard origin/master
+
+# Use this for local testing:
+# git checkout 89f316ab7bb7a6779b68709a1d7d7824397d2870
+
+git fetch --all
+git reset --hard origin/master
 
 # Install dependencies
 echo "Installing npm dependencies .."
