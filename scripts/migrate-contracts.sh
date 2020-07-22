ee1a91cf25073f8a091e0f5ab02d6d8040369605
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2020-07-22 10:58:45 +0200

    Use older contract versions for now

--- scripts/migrate-contracts.sh
+++ scripts/migrate-contracts.sh
@@ -10,11 +10,9 @@ check_tmp_folder $REPOSITORY $FOLDER_NAME
 # Clean up
 rm -rf build
 
-# Use this for local testing:
-# git checkout 89f316ab7bb7a6779b68709a1d7d7824397d2870
-
-git fetch --all
-git reset --hard origin/master
+git checkout 21296095382a856a32484aaf48812b78f3aeda6d
+# git fetch --all
+# git reset --hard origin/master
 
 # Install dependencies
 echo "Installing npm dependencies .."
