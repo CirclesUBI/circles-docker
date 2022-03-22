c0592249ee810f8252775f19dcc8002a83173ae4
jacque <jacque@jacques-MBP.fritz.box>
2022-03-22 18:05:00 +0100

    fix common script

--- scripts/deploy-subgraph.sh
+++ scripts/deploy-subgraph.sh
@@ -12,7 +12,7 @@ check_tmp_folder $REPOSITORY $FOLDER_NAME
 rm -rf build src/types
 git fetch --all
 
-git reset --hard v1.2.1-5-g935723a
+git reset --hard v1.2.1-6-g6bb06c7
 
 
 # Install dependencies
