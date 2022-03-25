cdf961ceabd63a6f120ad40fe7778f39bb062539
JacqueGM <55389166+JacqueGM@users.noreply.github.com>
2022-03-25 15:05:56 +0100

    Fix core tests (#40)
    * Update subgraph to v1.2.2
    
    Co-authored-by: llunaCreixent <elesmp@protonmail.com>
    Co-authored-by: jacque <jacque@jacques-MBP.fritz.box>

--- scripts/deploy-subgraph.sh
+++ scripts/deploy-subgraph.sh
@@ -12,7 +12,7 @@ check_tmp_folder $REPOSITORY $FOLDER_NAME
 rm -rf build src/types
 git fetch --all
 
-git reset --hard v1.2.0
+git reset --hard v1.2.2
 
 
 # Install dependencies
@@ -27,9 +27,9 @@ export IPFS_NODE_ENDPOINT=http://localhost:5001
 export GRAPH_ADMIN_NODE_ENDPOINT=http://localhost:8020
 
 # Prepare subgraph
-npm run codegen --loglevel verbose
-npm run build --loglevel verbose
+npm run codegen
+npm run build
 
 # Create and deploy subgraph to node
-npm run create --loglevel verbose
-npm run deploy --loglevel verbose
+npm run create
+npm run deploy
