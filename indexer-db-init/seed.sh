8cdb589cb2c5828b31b801e0c1db9dfb244bcce8
JacqueGM <jacquemate@riseup.net>
2023-02-02 16:56:50 +0100

    add otheruser blockchain

--- indexer-db-init/seed.sh
+++ indexer-db-init/seed.sh
@@ -4,7 +4,7 @@ cd blockchain-indexer
 git checkout dev
 
 export CONNECTION_STRING_ROOT="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
-
+echo $CONNECTION_STRING_ROOT
 ls CirclesLand.BlockchainIndexer/DbMigrations/*.sql | xargs -I% sh -c 'psql ${CONNECTION_STRING_ROOT} << envsubst < %'
 
 cd ..
