31f4ea25e42c8841c9026f2999573dacd2779f60
JacqueGM <jacquemate@riseup.net>
2023-02-27 22:31:05 +0100

    update -i file for linux

--- scripts/update_contract_addresses.sh
+++ scripts/update_contract_addresses.sh
@@ -12,5 +12,5 @@ while read -r line; do  addresses+=("$line"); done < $SCRIPT_DIR/../.tmp/contrac
 ENV_FILE=$SCRIPT_DIR/../.env
 
 # Substitute in .env in circles-docker  
-sed -i '.bak'  's/HUB_ADDRESS=[^ ]*/HUB_ADDRESS='"${addresses[0]}"'/; s/PROXY_FACTORY_ADDRESS=[^ ]*/PROXY_FACTORY_ADDRESS='"${addresses[6]}"'/;s/SAFE_ADDRESS=[^ ]*/SAFE_ADDRESS='"${addresses[8]}"'/;s/MULTISEND_ADDRESS=[^ ]*/MULTISEND_ADDRESS='"${addresses[3]}"'/;s/MULTSENDCALL_ADDRESS=[^ ]*/MULTSENDCALL_ADDRESS='"${addresses[5]}"'/;s/SAFE_DEFAULT_CALLBACK_HANDLER=[^ ]*/SAFE_DEFAULT_CALLBACK_HANDLER='"${addresses[7]}"'/;s/PROXY_FACTORY_ADDRESS_CRC=[^ ]*/PROXY_FACTORY_ADDRESS_CRC='"${addresses[2]}"'/;s/SAFE_CONTRACT_ADDRESS_CRC=[^ ]*/SAFE_CONTRACT_ADDRESS_CRC='"${addresses[1]}"'/' $ENV_FILE
+sed -i  's/HUB_ADDRESS=[^ ]*/HUB_ADDRESS='"${addresses[0]}"'/; s/PROXY_FACTORY_ADDRESS=[^ ]*/PROXY_FACTORY_ADDRESS='"${addresses[6]}"'/;s/SAFE_ADDRESS=[^ ]*/SAFE_ADDRESS='"${addresses[8]}"'/;s/MULTISEND_ADDRESS=[^ ]*/MULTISEND_ADDRESS='"${addresses[3]}"'/;s/MULTSENDCALL_ADDRESS=[^ ]*/MULTSENDCALL_ADDRESS='"${addresses[5]}"'/;s/SAFE_DEFAULT_CALLBACK_HANDLER=[^ ]*/SAFE_DEFAULT_CALLBACK_HANDLER='"${addresses[7]}"'/;s/PROXY_FACTORY_ADDRESS_CRC=[^ ]*/PROXY_FACTORY_ADDRESS_CRC='"${addresses[2]}"'/;s/SAFE_CONTRACT_ADDRESS_CRC=[^ ]*/SAFE_CONTRACT_ADDRESS_CRC='"${addresses[1]}"'/' $ENV_FILE
 
