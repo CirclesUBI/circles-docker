8f4902d07bc2a7a4a527026f849bda2a1d4e4cad
JacqueGM <jacquemate@riseup.net>
2023-02-27 22:44:41 +0100

    add compatibility sed for both mac and linux

--- scripts/update_contract_addresses.sh
+++ scripts/update_contract_addresses.sh
@@ -11,6 +11,11 @@ while read -r line; do  addresses+=("$line"); done < $SCRIPT_DIR/../.tmp/contrac
 
 ENV_FILE=$SCRIPT_DIR/../.env
 
-# Substitute in .env in circles-docker  
-sed -i  's/HUB_ADDRESS=[^ ]*/HUB_ADDRESS='"${addresses[0]}"'/; s/PROXY_FACTORY_ADDRESS=[^ ]*/PROXY_FACTORY_ADDRESS='"${addresses[6]}"'/;s/SAFE_ADDRESS=[^ ]*/SAFE_ADDRESS='"${addresses[8]}"'/;s/MULTISEND_ADDRESS=[^ ]*/MULTISEND_ADDRESS='"${addresses[3]}"'/;s/MULTSENDCALL_ADDRESS=[^ ]*/MULTSENDCALL_ADDRESS='"${addresses[5]}"'/;s/SAFE_DEFAULT_CALLBACK_HANDLER=[^ ]*/SAFE_DEFAULT_CALLBACK_HANDLER='"${addresses[7]}"'/;s/PROXY_FACTORY_ADDRESS_CRC=[^ ]*/PROXY_FACTORY_ADDRESS_CRC='"${addresses[2]}"'/;s/SAFE_CONTRACT_ADDRESS_CRC=[^ ]*/SAFE_CONTRACT_ADDRESS_CRC='"${addresses[1]}"'/' $ENV_FILE
 
+# Substitute in .env in circles-docker  
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sed -i  's/HUB_ADDRESS=[^ ]*/HUB_ADDRESS='"${addresses[0]}"'/; s/PROXY_FACTORY_ADDRESS=[^ ]*/PROXY_FACTORY_ADDRESS='"${addresses[6]}"'/;s/SAFE_ADDRESS=[^ ]*/SAFE_ADDRESS='"${addresses[8]}"'/;s/MULTISEND_ADDRESS=[^ ]*/MULTISEND_ADDRESS='"${addresses[3]}"'/;s/MULTSENDCALL_ADDRESS=[^ ]*/MULTSENDCALL_ADDRESS='"${addresses[5]}"'/;s/SAFE_DEFAULT_CALLBACK_HANDLER=[^ ]*/SAFE_DEFAULT_CALLBACK_HANDLER='"${addresses[7]}"'/;s/PROXY_FACTORY_ADDRESS_CRC=[^ ]*/PROXY_FACTORY_ADDRESS_CRC='"${addresses[2]}"'/;s/SAFE_CONTRACT_ADDRESS_CRC=[^ ]*/SAFE_CONTRACT_ADDRESS_CRC='"${addresses[1]}"'/' $ENV_FILE
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '.bak'  's/HUB_ADDRESS=[^ ]*/HUB_ADDRESS='"${addresses[0]}"'/; s/PROXY_FACTORY_ADDRESS=[^ ]*/PROXY_FACTORY_ADDRESS='"${addresses[6]}"'/;s/SAFE_ADDRESS=[^ ]*/SAFE_ADDRESS='"${addresses[8]}"'/;s/MULTISEND_ADDRESS=[^ ]*/MULTISEND_ADDRESS='"${addresses[3]}"'/;s/MULTSENDCALL_ADDRESS=[^ ]*/MULTSENDCALL_ADDRESS='"${addresses[5]}"'/;s/SAFE_DEFAULT_CALLBACK_HANDLER=[^ ]*/SAFE_DEFAULT_CALLBACK_HANDLER='"${addresses[7]}"'/;s/PROXY_FACTORY_ADDRESS_CRC=[^ ]*/PROXY_FACTORY_ADDRESS_CRC='"${addresses[2]}"'/;s/SAFE_CONTRACT_ADDRESS_CRC=[^ ]*/SAFE_CONTRACT_ADDRESS_CRC='"${addresses[1]}"'/' $ENV_FILE
+fi
