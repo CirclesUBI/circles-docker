c580b673079e4cccf13348d45d638273196c3f50
ele <lluna@bitspossessed.org>
2023-05-18 22:53:02 +0200

    Use circles-contracts testing branch

--- scripts/migrate-contracts.sh
+++ scripts/migrate-contracts.sh
@@ -13,7 +13,7 @@ check_tmp_folder $REPOSITORY $FOLDER_NAME
 rm -rf build
 
 git fetch --all
-git reset --hard v3.3.1
+git checkout node16
 
 # Install dependencies
 echo "Installing npm dependencies .."
